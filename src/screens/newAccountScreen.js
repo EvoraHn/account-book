f7d98a545be8181bbaec97933b3414184a4e79e9
EvoraHn <56361891+EvoraHn@users.noreply.github.com>
2020-11-29 11:18:15 -0600

    Estructura base con importaciones para la pantalla de nueva nota

---
+++ src/screens/newAccountScreen.js
@@ -0,0 +1,26 @@
+import { Form } from "native-base";
+import React from "react";
+import{Container,Text,Card,CardItem} from "native-base";
+import
+    {StyleSheet,Image,Dimensions} 
+from "react-native";
+const newAccountScreen =() => {
+    return (
+        <Container>
+
+
+
+
+        </Container>
+    )
+} 
+
+const styles = StyleSheet.create({
+    main:{
+        marginTop:32,
+        flex:1,
+
+    }
+
+})
+export default newAccountScreen;
