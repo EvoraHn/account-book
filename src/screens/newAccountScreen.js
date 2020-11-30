2c008590c0da8f9db5a18c64b17568a30d687f25
EvoraHn <56361891+EvoraHn@users.noreply.github.com>
2020-11-29 22:40:20 -0600

    diseño de estructura "Colors" de pantalla nueva cuenta

--- src/screens/newAccountScreen.js
+++ src/screens/newAccountScreen.js
@@ -1,13 +1,30 @@
 import React from "react";
-import{Container,Text,Card,CardItem} from "native-base";
+import{Container,Text,Card,CardItem,Body,Button} from "native-base";
 import{StyleSheet,Image,Dimensions} from "react-native";
-const newAccountScreen =() => {
+const newAccountScreen =({navigation}) => {
     return (
         <Container>
+            <Container style={styles.verticalContainer}>
 
+                <Container style={styles.verticalbar}>
+                   <Card style={styles.verticalBarContent}>
+                        <Text style={styles.verticalBarText} >
+                           Nueva Cuenta
+                        </Text>
+                   </Card>
+               </Container>
 
-
-
+               <Container style={styles.horizontalContainer}>
+                    <Card transparent>
+                        
+                    </Card>
+                    
+                    
+               </Container>
+               
+               
+               
+            </Container>
         </Container>
     )
 } 
@@ -17,7 +34,40 @@ const styles = StyleSheet.create({
         marginTop:32,
         flex:1,
 
-    }
+    },
+    verticalbar:{
+        flex:.10,
+        alignSelf:"center",
+        justifyContent:"center"
+    },
+    verticalBarText:{ 
+        transform: [{ rotate: '90deg'}],
+        color:"white",
+        fontSize:30,
+        width:200,
+        alignSelf:"flex-end",
+        marginRight:-30
+    },
+    verticalBarContent:{
+        borderRadius:30,
+        height:600,
+        marginLeft:-30,
+        marginRight:5,
+        //marginTop:100,
+        backgroundColor:'blue',
+        textAlign:"center",
+        justifyContent:"center",
+    },
+    verticalContainer:{
+        flexDirection:'row',
+        backgroundColor:'green',
+    },
+    horizontalContainer:{
+        width:10,
+        flexDirection:'column',
+        backgroundColor:'pink',
+       
+    },
 
 })
 export default newAccountScreen;
