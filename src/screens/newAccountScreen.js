8ae26e2ed19c4acc0bb975a4fc76f7d9ae208ba1
EvoraHn <56361891+EvoraHn@users.noreply.github.com>
2020-11-30 10:37:15 -0600

    structura de los inputs , la barra vertical y el contenedor para la calculadora

--- src/screens/newAccountScreen.js
+++ src/screens/newAccountScreen.js
@@ -1,26 +1,67 @@
 import React from "react";
-import{Container,Text,Card,CardItem,Body,Button} from "native-base";
+import{Container,Text,Card,CardItem,Body,Button,Input,Item, Left, Right} from "native-base";
 import{StyleSheet,Image,Dimensions} from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
 const newAccountScreen =({navigation}) => {
     return (
-        <Container>
+        <Container style={styles.main}>
             <Container style={styles.verticalContainer}>
 
                 <Container style={styles.verticalbar}>
-                   <Card style={styles.verticalBarContent}>
-                   </Card>
-               </Container>
+                    <Card style={styles.verticalBarContent} >
+                    </Card>
+                    <Button onPress={()=>
+                                {navigation.navigate("counsBookHomeScreen")}}>
+                    </Button>
+                </Container>
 
                <Container style={styles.horizontalContainer}>
-                    <Card transparent>
-                        
+                    <Card  style={styles.inputBar}>
+                        <Item >
+                            <Input placeholder={"Motivo de la cuenta"}/>
+                           
+                        </Item>
                     </Card>
-                    
-                    
+                    <Item>
+                        <Left > 
+                            <Button style={styles.Button} >
+                                <Text >       
+                                </Text>
+                            </Button>
+                        </Left>
+                        <Right >
+                            <Card  style={styles.inputBarSecondary} >
+                                <Item>
+                                    <Input placeholder={"Acreedor de la cuenta"}/>
+                                </Item>
+                            </Card>
+                        </Right>
+                    </Item>
+
+                    <Card  style={styles.calculatorContainer}>
+                        
+                            <Input placeholder={"0.00"}/>
+                            <Input placeholder={"Total"}/>
+                            <Card >
+
+                            </Card>
+                    </Card>
+
+                    <Card style={styles.comentaryContainer}>
+                        <Input placeholder={"Comentario"} />
+                    </Card>
+
+                    <Button transparent style={{alignSelf:"flex-end",marginTop:30}} > 
+                        <Card style={styles.addButton}>
+                            <Text>Agregar</Text>
+                            <Text>Nota</Text>
+
+                        </Card>
+                        
+                    </Button>
                </Container>
-               
-               
-               
             </Container>
         </Container>
     )
@@ -30,6 +71,7 @@ const styles = StyleSheet.create({
     main:{
         marginTop:32,
         flex:1,
+        
 
     },
     verticalbar:{
@@ -63,8 +105,54 @@ const styles = StyleSheet.create({
         width:10,
         flexDirection:'column',
         backgroundColor:'pink',
-       
     },
+    inputBar:{
+        //backgroundColor:'white',
+        borderRadius:30,
+        //flex:.12,
+        justifyContent:"center",
+        width:width*.70,
+        alignSelf:"flex-end"
+    },
+    inputBarSecondary:{
+        //backgroundColor:'white',
+        borderRadius:30,
+        //flex:.12,
+        justifyContent:"center",
+        width:width*.60,
+        alignSelf:"flex-end"
+    },
+    Button:{
+        backgroundColor:'green',
+        width:width*.11,
+        borderRadius:50,
+        marginLeft:30
+    },
+    calculatorContainer:{
+        borderRadius:30,
+        flex:.5,
+        justifyContent:"center",
+        width:width*.60,
+        alignSelf:"center"
+    },
+    comentaryContainer:{
+        borderRadius:30,
+        flex:.3,
+        justifyContent:"center",
+        width:width*.80,
+        alignSelf:"center"
+    },
+    addButton:{
+        backgroundColor:'green',
+        width:width*.30,
+        height:70,
+        textAlign:"center",
+        justifyContent:"center",
+        alignItems:"center",
+        borderRadius:20,
+        marginRight:22
+    }
+
 
 })
 export default newAccountScreen;
