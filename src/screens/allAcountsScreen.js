e3902f746966385ef6106a40ef4e764676436f05
EvoraHn <56361891+EvoraHn@users.noreply.github.com>
2020-11-29 20:11:31 -0600

    Estructura para pantalla todas las cuentas

---
+++ src/screens/allAcountsScreen.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+import{StyleSheet,Image,Dimensions} from "react-native";
+import{Container,Text,Card,CardItem} from "native-base";
+
+const allAccountsScreen =() => {
+    return (
+        <Container>
+
+
+
+
+        </Container>
+    )
+} 
+
+const styles = StyleSheet.create({
+    main:{
+        marginTop:32,
+        flex:1,
+    }
+
+})
+export default allAccountsScreen;
