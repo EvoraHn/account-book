54acd8356c0b471bf6e2bd303f40988c4f2d6132
EvoraHn <56361891+EvoraHn@users.noreply.github.com>
2020-12-01 22:38:48 -0600

    Estructura base para la pantalla AllAccountsScreen

--- src/screens/allAcountsScreen.js
+++ src/screens/allAcountsScreen.js
@@ -1,16 +1,27 @@
 
 import React from "react";
 import{StyleSheet,Image,Dimensions} from "react-native";
-import{Container,Text,Card,CardItem} from "native-base";
+import{Container,Text,Card,CardItem,Button} from "native-base";
 
-const allAccountsScreen =() => {
+const allAccountsScreen =({navigation}) => {
     return (
-        <Container>
+        <Container style={styles.main}>
+            <Container style={styles.verticalContainer}>
 
+                <Container style={styles.verticalbar}>
+                    
+                    <Button style={styles.verticalBarContent} onPress={()=>
+                                {navigation.navigate("countsBookHomeScreen")}}>
+                                    <Text/>
+                                    
+                    </Button>
+                </Container>
 
-
-
+               <Container style={styles.horizontalContainer}>
+               </Container>
+            </Container>
         </Container>
+               
     )
 } 
 
@@ -18,7 +29,39 @@ const styles = StyleSheet.create({
     main:{
         marginTop:32,
         flex:1,
-    }
-
+    },
+    verticalbar:{
+        flex:.10,
+        alignSelf:"center",
+        justifyContent:"center"
+    },
+    verticalBarText:{ 
+        transform: [{ rotate: '90deg'}],
+        color:"white",
+        fontSize:30,
+        width:200,
+        alignSelf:"flex-end",
+        marginRight:-30
+    },
+    verticalBarContent:{
+        flex:.7,
+        borderRadius:30,
+        width:50,
+        marginLeft:-30,
+        marginRight:5,
+        //marginTop:100,
+        backgroundColor:'blue',
+        textAlign:"center",
+        justifyContent:"center",
+    },
+    verticalContainer:{
+        flexDirection:'row',
+        //backgroundColor:'green',
+    },
+    horizontalContainer:{
+        width:10,
+        flexDirection:'column',
+        backgroundColor:'pink',
+    },
 })
 export default allAccountsScreen;
