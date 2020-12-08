f6ed0bd5c6a3cce8c60e2aee4c5825c924c20ae8
EvoraHn <56361891+EvoraHn@users.noreply.github.com>
2020-12-08 11:10:51 -0600

    Cambio de colores ("paleta de colores") Pantalla principal

--- src/screens/countsBookHomeScreen.js
+++ src/screens/countsBookHomeScreen.js
@@ -8,26 +8,26 @@ const countsBookhomeScreen = ({navigation}) => {
     return (
         <Container style={styles.main}>
             
-            <CardItem> 
+            <CardItem transparent style={{backgroundColor:'#FAF8DC',}}> 
                     <Left>
                         <Text style={styles.title}>Account</Text>
                     </Left>   
                     
                     <Right>
-                        <Image source={require("../img/AccountsBook.jpg")}
+                        <Image source={require("../img/AccountsBook.png")}
                          transparent style={styles.ImageLogo}/>
                     </Right>
-                    </CardItem>
-                    <CardItem >
-                        <Left>
-                            <Text style={styles.subtitle}>Book</Text>
-                        </Left>
-                    </CardItem>
+            </CardItem >
+            <CardItem transparent style={{backgroundColor:'#FAF8DC',}} >
+                <Left>
+                    <Text style={styles.subtitle}>Book</Text>
+                </Left>
+            </CardItem>
             
            <Container style={styles.verticalContainer}>
 
                <Container style={styles.horizontalContainer}>
-                   <ScrollView horizontal={true}>
+                   <ScrollView horizontal={true} style={{height:10}}>
                    <Card transparent >
                         <CardItem style={styles.cardItem}>
                             <Button transparent style={styles.buttonContent}>
@@ -69,6 +69,14 @@ const countsBookhomeScreen = ({navigation}) => {
                         </CardItem> 
                     </Card>
                    </ScrollView>
+                   <Card transparent style={{height:500,alignItems:"center",backgroundColor:'#FAF8DC',}}>
+                        <CardItem transparent style={{backgroundColor:'#FAF8DC',}}>
+                            <Button transparent >
+                                    <Image source={require("../img/image.png")}
+                                    transparent style={{height:width*.7,width:width*.7,backgroundColor:'#FAF8DC',}}/>
+                            </Button>
+                        </CardItem> 
+                    </Card>
                </Container>
                
                <Container style={styles.verticalbar}>
@@ -93,42 +101,43 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent:"center",
         alignItems:"center",
+        backgroundColor:'#FAF8DC',
     },
     verticalContainer:{
         flexDirection:'row',
-        backgroundColor:'green',
-        
+        backgroundColor:'#FAF8DC',
     },
     horizontalContainer:{
         width:10,
         flexDirection:'column',
-        //backgroundColor:'pink',
-       
+        backgroundColor:'#FAF8DC',
     },
     ImageLogo:{
         width:150,
         height:150,
         resizeMode: "contain",
+        backgroundColor:'#FAF8DC',
     },
     title:{
         marginTop:-50,
         fontSize:50,
+        backgroundColor:'#FAF8DC',
     },
     subtitle:{
         marginTop:-100,
         fontSize:50,
+        backgroundColor:'#FAF8DC',
     },
     verticalbar:{
         flex:.20, 
+        backgroundColor:'#FAF8DC',
        },
     verticalBarText:{ 
         transform: [{ rotate: '270deg'}],
         color:"white",
         fontSize:30,
         width:250,
-        //height:300,
-        //alignSelf:"flex-end",
-        marginRight:20
+        marginRight:20,
     },
     verticalBarContent:{
         borderRadius:30,
@@ -137,20 +146,21 @@ const styles = StyleSheet.create({
         marginRight:-30,
         marginLeft:5,
         //marginTop:30,
-        backgroundColor:'blue',
+        backgroundColor:'#111E6C',
         textAlign:"center",
         justifyContent:"center",
     },
     mainContent:{
         alignItems:"center",
         //justifyContent:"center",
+        //backgroundColor:'#FAF8DC',
     },
     cardItem:{
         borderRadius:30,
-        backgroundColor:'grey'
+        backgroundColor:'#cdcdcd'
     },
     buttonContent:{
-        backgroundColor:'grey',
+        backgroundColor:'#cdcdcd',
         borderStyle:"dotted",
         height:200,
         alignItems:"center",
